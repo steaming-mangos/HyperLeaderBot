066b1220b6fc05614e3788e03110d2ced7c70f03
Steaming_Mangos <37495286+steaming-mangos@users.noreply.github.com>
2023-09-16 04:30:23 -0400

    Update HyperLeaderbot.py
    Added features to make new 400 posts automatically, update top 25 roles automatically, and reformatted various other parts of the code

--- HyperLeaderbot.py
+++ HyperLeaderbot.py
@@ -1,5 +1,11 @@
-from discord.ext import commands
+from discord.ext import commands, tasks
 import discord
+import json
+import time
+import shutil
+import datetime
+from leaderboard_scraper import *
+from pb_img_gen import gen_pb
 
 with open("bot_token.txt") as file:
     BOT_TOKEN = file.read()
@@ -39,9 +45,255 @@ top_roles = [
     ["Top 25", 1142897909029945394]
 ]
 
-wr_holder_id = 1142895896372203550
+wr_role_id = 1142895896372203550
 
 bot = commands.Bot(command_prefix="+", intents=discord.Intents.all())
+
+def update_dict(key, value, source_dict):
+    with open(source_dict, "r") as json_file:
+        my_dict = json.load(json_file)
+
+    # make changes to myDict
+    my_dict[key] = value
+
+    with open(source_dict, "w") as json_file:
+        json.dump(my_dict, json_file)
+
+def ordinal(value):
+    ordval = f'{value}'
+    if value % 100//10 != 1:
+        if value % 10 == 1:
+            ordval += 'st'
+        elif value % 10 == 2:
+            ordval += 'nd'
+        elif value % 10 == 3:
+            ordval += 'rd'
+        else:
+            ordval += 'th'
+    else:
+        ordval += 'th'
+
+    return ordval
+
+# leaderboard update funtion
+def lb_update():
+    #set old leaderboard as new one
+    shutil.copyfile('lb_dict_new.json', 'lb_dict_old.json')
+
+    lb_array = get_shit()
+    lb_dict_new = {}
+    key_list = []
+    for x in lb_array:
+        user_values = {}
+        user_values["rank"] = x[0]
+        user_values["username"] = x[1]
+        user_values["score"] = x[3]
+        user_values["link"] = x[4]
+        lb_dict_new[x[2]] = user_values
+        key_list.append([x[3], x[2]])
+
+    # update the new leaderboard dict
+    with open("lb_dict_new.json", "w") as outfile:
+        json.dump(lb_dict_new, outfile)
+    
+    # update the key list 
+    with open("key_list.json", "w") as outfile:
+        json.dump(key_list, outfile)
+
+
+# 400 post function
+@bot.command()
+async def auto_400_post(hdnews: discord.channel):
+    # post format
+    # Congratulations to *@user* for getting a new PB of *score*! They beat their old PB of *score_old* (*score_dif*), gaining *rank_dif* ranks. They are the *x* 400 player!
+
+    # open the id dictionary for reference
+    with open("id_dictionary.json", "r") as outfile:
+        id_dict = json.load(outfile)
+    with open("lb_dict_new.json", "r") as outfile:
+        lb_dict_new = json.load(outfile)
+    with open("lb_dict_old.json", "r") as outfile:
+        lb_dict_old = json.load(outfile)
+    with open("key_list.json", "r") as outfile:
+        key_list = json.load(outfile)
+
+    # compare old and new leaderboards to see if there are any new 400 scores
+    x = 0
+    while float(key_list[x][0]) >= 400:
+        user_key = key_list[x][1]
+        # check if there is a new PB
+        if round(float(lb_dict_new[user_key]["score"]), 4) > round(float(lb_dict_old[user_key]["score"]), 4):
+
+            # need to pull @username, new score, old score, change in score, rank changed, if new 400 player]]
+            user_id = id_dict.get(key_list[x][1])
+            username = lb_dict_new[user_key]["username"]
+            score_new = round(float(lb_dict_new[user_key]["score"]), 3)
+            score_old = round(float(lb_dict_old[user_key]["score"]), 3)
+            rank_new = int(lb_dict_new[user_key]["rank"])
+            rank_old = int(lb_dict_old[user_key]["rank"])
+            score_dif = score_new - score_old
+            rank_change = rank_new - rank_old
+            howmany400 = int
+            if score_old < 400:
+                new_400 = True
+                score_list = [sublist[0] for sublist in key_list]
+                howmany400 = sum(float(score) >= 400 for score in score_list)
+            else:
+                new_400 = False
+                
+            # send news post
+            if user_id == None and new_400 == False:
+                await hdnews.send(f"Congratulations to {username} for getting a new PB of {score_new}! They beat their old PB of {score_old} (+{score_dif}), gaining {rank_change} ranks.")
+                gen_pb('400_test_image.png', str(rank_new), str(username), str(score_new))
+                await hdnews.send(file=discord.File('400_test_image.png'))
+                time.sleep(2)
+            elif user_id == None and new_400 == True:
+                await hdnews.send(f"Congratulations to {username} for getting a new PB of {score_new}! They beat their old PB of {score_old} (+{score_dif}), gaining {rank_change} ranks. They are the {ordinal(howmany400)} 400 player!")
+                gen_pb('400_test_image.png', str(rank_new), str(username), str(score_new))
+                await hdnews.send(file=discord.File('400_test_image.png'))
+                time.sleep(2)
+            elif user_id != None and new_400 == False:
+                await hdnews.send(f"Congratulations to <@{user_id}> for getting a new PB of {score_new}! They beat their old PB of {score_old} (+{score_dif}), gaining {rank_change} ranks.")
+                gen_pb('400_test_image.png', str(rank_new), str(username), str(score_new))
+                await hdnews.send(file=discord.File('400_test_image.png'))
+                time.sleep(2)
+            elif user_id != None and new_400 == True:
+                await hdnews.send(f"Congratulations to <@{user_id}> for getting a new PB of {score_new}! They beat their old PB of {score_old} (+{score_dif}), gaining {rank_change} ranks. They are the {ordinal(howmany400)} 400 player!")
+                gen_pb('400_test_image.png', str(rank_new), str(username), str(score_new))
+                await hdnews.send(file=discord.File('400_test_image.png'))
+                time.sleep(2)
+            
+        x += 1
+        
+
+
+# role update function
+@bot.command()
+async def top_role_update(ctx):
+
+    op = ctx.guild.get_member(ctx.interaction.user.id)
+
+    invoker_roles_before = op.roles
+
+    with open("lb_dict_new.json", "r") as outfile:
+        lb_dict_new = json.load(outfile)
+    with open("lb_dict_old.json", "r") as outfile:
+        lb_dict_old = json.load(outfile)
+    with open("id_dictionary.json", "r") as outfile:
+        id_dict = json.load(outfile)
+    with open("key_list.json", "r") as outfile:
+        key_list = json.load(outfile)
+
+    top_3_user_id = []
+    top_10_user_id = []
+    top_25_user_id = []
+
+    wr_role = ctx.guild.get_role(wr_role_id)
+    top_3_role = ctx.guild.get_role(top_roles[0][1])
+    top_10_role = ctx.guild.get_role(top_roles[1][1])
+    top_25_role = ctx.guild.get_role(top_roles[2][1])
+    
+    # check rank 1 and see if there is a matching id
+    wr_user_id = id_dict.get(key_list[0][1])
+    # append top_3_id list with discord id's from id_dictionary
+    for x in key_list[1:3]:
+        top_3_user_id.append(id_dict.get(x[1]))
+    # append top_10_id list with discord id's from id_dictionary
+    for x in key_list[3:10]:
+        top_10_user_id.append(id_dict.get(x[1]))
+    # append top_25_id list with discord id's from id_dictionary
+    for x in key_list[10:25]:
+        top_25_user_id.append(id_dict.get(x[1]))
+
+    # remove roles from anyone not in top lists
+    for user in ctx.guild.members:
+        if wr_role in user.roles and user.id != wr_user_id:
+            await user.remove_roles(wr_role)
+        if top_3_role in user.roles and user.id not in top_3_user_id:
+            await user.remove_roles(top_3_role)
+        if top_10_role in user.roles and user.id not in top_10_user_id:
+            await user.remove_roles(top_10_role)
+        if top_25_role in user.roles and user.id not in top_25_user_id:
+            await user.remove_roles(top_25_role)
+    
+    # check wr id and update if necessary
+    wr_user = ctx.guild.get_member(wr_user_id)
+    if wr_user == None:
+        pass
+    else:
+        await wr_user.add_roles(wr_role)
+
+    # check top 3 ids and update if necessary
+    for user in top_3_user_id:
+        if user == None:
+            pass
+        # assign user top 3 role
+        else:
+            top_3_user = ctx.guild.get_member(user)
+            await top_3_user.add_roles(top_3_role)
+
+    # check top 10 ids and update if necessary
+    for user in top_10_user_id:
+        if user == None:
+            pass
+        else:
+            top_10_user = ctx.guild.get_member(user)
+            await top_10_user.add_roles(top_10_role)
+
+    # check top 25 ids and update if necessary
+    for user in top_25_user_id:
+        if user == None:
+            pass
+        else:
+            top_25_user = ctx.guild.get_member(user)
+            await top_25_user.add_roles(top_25_role)
+
+    invoker_roles_after = op.roles
+
+    before_set = set(invoker_roles_before)
+    after_set = set(invoker_roles_after)
+
+    added_roles = []
+    removed_roles = []
+
+    if before_set == after_set:
+        # no roles were changed
+        pass
+    elif before_set.issubset(after_set):
+        # this means roles were added
+        for role in invoker_roles_after:
+            if role not in invoker_roles_before:
+                added_roles.append(role.id)
+    elif after_set.issubset(before_set):
+        # this means roles were removed
+        for role in invoker_roles_before:
+            if role not in invoker_roles_after:
+                removed_roles.append(role.id)
+    else:
+        # this means roles were both added and removed
+        for role in invoker_roles_after:
+            if role not in invoker_roles_before:
+                added_roles.append(role.id)
+        for role in invoker_roles_before:
+            if role not in invoker_roles_after:
+                removed_roles.append(role.id)
+
+    return added_roles, removed_roles
+
+# check for roles and new 400 pb's every 5 minutes
+@tasks.loop(minutes = 5)
+async def maintain_hdpals(channel):
+#async def maintain_hdpals(channel, guild):
+    lb_update()
+    #await top_role_update(guild)
+    await auto_400_post(channel)
+
+@bot.event
+async def on_ready():
+    channel = bot.get_channel(1047631851193368697)
+    #guild = bot.get_guild(1141412260217114694)
+    #maintain_hdpals.start(channel, guild)
+    maintain_hdpals.start(channel)
 
 # function for adding a role
 @bot.command()
@@ -63,10 +315,20 @@ async def on_message(message):
     if message.author == bot.user:
         return
 
+    # register command
+    if message.content.startswith('register'):
+        register_id = str(message.content.split()[1])
+        update_dict(register_id, message.user.id, 'id_dictionary.json')
+
     # checks if message is from SORATH bot, and it is the "/hyperdemon pb" command
     if message.author.id == 798042141988618251 and message.interaction.name == "hyperdemon pb":
+
+        lb_update()
+        added_roles, removed_roles = await top_role_update(message)
+        await auto_400_post(bot.get_channel(1047631851193368697))
+
         embed_description = message.embeds[0].description
-        embed_op_id = message.interaction.user.id
+        embed_op_id = int(message.interaction.user.id)
         embed_type = message.interaction.name
         description = embed_description.splitlines()
        
@@ -74,7 +336,10 @@ async def on_message(message):
         score_new = round(float(description[1].split()[1].strip("*")), 3)
         score_dif = round(float(description[1].split()[2].strip("()+")), 3)
         score_old = round(score_new-score_dif, 3)
-        rank = description[0].split()[1].strip("*")
+        rank = int(description[0].split()[1].strip("*"))
+
+        # update id dictionary
+        update_dict(get_shit()[rank-1][2], embed_op_id, 'id_dictionary.json')
 
         # find users old role
         old_role_id = "no score assigned"
@@ -106,31 +371,43 @@ async def on_message(message):
         # check if this is their first time triggering bot and update role
         if old_role_id == "no score assigned":
             await add_role(message, new_role_id)
+            added_roles.append(new_role_id)
             embed_role_update = discord.Embed(title=f"Updated roles for {message.interaction.user.name}")
-            embed_role_update.add_field(name="**Added**", value=f"<@&{new_role_id}>", inline=True)
+            if added_roles != []:
+                addstring = f""
+                for addedroleid in added_roles:
+                    addstring = f"{addstring}<@&{addedroleid}>\n"
+                embed_role_update.add_field(name="**Added:**", value=addstring, inline=True)
             await message.channel.send(embed=embed_role_update)
         
         # check if new role is the same as old role and do nothing
         elif old_role_id == new_role_id:
-            if next_score != None:
+            if next_score != []:
                 dist_next_role = round(next_score[0]-score_new, 3)
                 await message.channel.send(f"No role change needed \n\n You're **{dist_next_role}** points away from the next role: <@&{next_score[1]}>")
             else:
                 await message.channel.send(f"You are a god, {message.interaction.user.name}. No higher role left")
 
-            
-
         # replace old role with new role
         else:
             # remove old role
             await remove_role(message, old_role_id)
+            removed_roles.append(old_role_id)
             # add new role
             await add_role(message, new_role_id)
+            added_roles.append(new_role_id)
             # post embed with role changes
             embed_role_update = discord.Embed(title=f"Updated roles for {message.interaction.user.name}")
-            embed_role_update.add_field(name="**Removed:**", value=f"<@&{old_role_id}>", inline=True)
-            embed_role_update.add_field(name="**Added**", value=f"<@&{new_role_id}>", inline=True)
+            if removed_roles != []:
+                removestring = f""
+                for removedroleid in removed_roles:
+                    removestring = f"{removestring}<@&{removedroleid}>\n"
+                embed_role_update.add_field(name="**Removed:**", value=removestring, inline=True)
+            if added_roles != []:
+                addstring = f""
+                for addedroleid in added_roles:
+                    addstring = f"{addstring}<@&{addedroleid}>\n"
+                embed_role_update.add_field(name="**Added:**", value=addstring, inline=True)
             await message.channel.send(embed=embed_role_update)
 
 bot.run(BOT_TOKEN)
-    
