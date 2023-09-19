32a70e8e166f43f9deea0bdf0052a750b261f639
Steaming_Mangos <37495286+steaming-mangos@users.noreply.github.com>
2023-09-19 15:31:13 -0400

    Update leaderboard_scraper.py
    Changed request into aiohttp, since request is blocking and aiohttp is not.

--- leaderboard_scraper.py
+++ leaderboard_scraper.py
@@ -1,23 +1,24 @@
 from bs4 import BeautifulSoup
-import requests
+import aiohttp
 
-def get_shit():
-    r = requests.get("https://hyprd.mn/leaderboards")
-    soup = BeautifulSoup(r.content)
-    t = soup.find('table', {'class:', 'leaderboard'})
+async def get_shit():
+    async with aiohttp.ClientSession() as session:
+        async with session.get("https://hyprd.mn/leaderboards") as r:
+            html = await r.text()
+            soup = BeautifulSoup(html, "html.parser")
+            t = soup.find('table', {'class:', 'leaderboard'})
 
-    # [[rank, username, hd_id, score, run_link], [.....]]
-    a = t.find_all('a')[0::2]
-    b = t.find_all('a')[1::2]
+            # [[rank, username, hd_id, score, run_link], [.....]]
+            a = t.find_all('a')[0::2]
+            b = t.find_all('a')[1::2]
 
-    lb = {}
-    # a is user id and username
-    # b is run id and score
-    for i in range(300):
-        uid = a[i].attrs.get("href").rfind("/")
-        uid = a[i].attrs.get("href")[uid+1::]
-        rlink = b[i].attrs.get("href").rfind("/")
-        rlink = b[i].attrs.get("href")[rlink + 1::]
-        lb[uid] = {'rank': i+1, 'username': a[i].text, 'score': b[i].text, 'run_link': rlink }
-
-    return lb
+            lb = {}
+            # a is user id and username
+            # b is run id and score
+            for i in range(300):
+                uid = a[i].attrs.get("href").rfind("/")
+                uid = a[i].attrs.get("href")[uid+1::]
+                rlink = b[i].attrs.get("href").rfind("/")
+                rlink = b[i].attrs.get("href")[rlink + 1::]
+                lb[uid] = {'rank': i+1, 'username': a[i].text, 'score': b[i].text, 'run_link': rlink }
+            return lb
